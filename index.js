1a0c5c7995757f699354217f10b62179e8cb347b
Eddy Dash <eddydashner2@gmail.com>
2023-05-30 18:54:23 +0200

    initialization

---
+++ index.js
@@ -0,0 +1,19 @@
+import  express  from "express";
+import mongoose from "mongoose";
+
+const connectToMongoDb=()=>{
+    mongoose.connect('mongodb+srv://mysaving2023:Ourfuture!@cluster0.vvdet8p.mongodb.net/?retryWrites=true&w=majority')
+    .then(()=>{
+        console.log("database connected succesfully");
+    }).catch(()=>{
+        console.log("failed to connect to the database")
+    })
+}
+
+const app =express();
+const port = 5500;
+
+app.listen(port,()=>{
+    console.log('server is running on port 5500')
+    connectToMongoDb();
+});
