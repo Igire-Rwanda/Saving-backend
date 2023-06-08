a10d26fc05e2285b1ee0c4e8841407f3d95a079f
23nosurrend <keynesbizimana@gmail.com>
2023-06-08 12:52:05 +0300

    I haved added function whihc is able to substract amount from bank account

--- controllers/transferController.js
+++ controllers/transferController.js
@@ -1,7 +1,45 @@
+import bankAccouts  from "../Models/bankModel.js"
 
 
-const transfer=(req,res)=>{
-    res.send("this is transfer")
+const transfer=async(req,res)=>{
+
+    try{
+      
+        const amount = req.body.amount;
+        const email = req.body.email;
+
+        const bankAccount=await bankAccouts.findOne({bankHolder:email})
+        if (!bankAccount) {
+            return res.status(400).json({ message: "Bank account not found" });
+          }
+        if(bankAccount.Amount <amount){
+            res.status(200).json({message:"inssufient balance"})
+        }
+      
+        else{
+            await bankAccouts.findOneAndUpdate(
+                {bankHolder:email},
+                {$inc:{Amount:-amount}}
+            )
+        
+        }
+        res.status(200).json(bankAccount.Amount)   
+        
+
+    }
+    catch(err){
+        console.log("err:",err),
+        res.status(500).json({message:"some error occured when accessing bank account"})
+
+    }
+
+
+
+
+
+
+
+    // res.send("this is transfer")
 }
 
 export default transfer;
