f68535deffbafa9d4d110b98ada16ad56be7eccd
Angelique Uwimbabazi <uwangela12@gmail.com>
2023-06-09 13:16:19 +0200

    fix deployment

--- Routes/expenseRoute.js
+++ Routes/expenseRoute.js
@@ -1,8 +1,8 @@
 import express from "express"
-import bodyParser from "body-parser";
+// import bodyParser from "body-parser";
 import {createExpense,readExpense} from "../Controllers/ExpenseController.js"
 const router = express.Router();
-router.use(bodyParser.json());
+// router.use(bodyParser.json());
 router.post("/create" ,createExpense );
 router.get("/read", readExpense);
 export default router;
