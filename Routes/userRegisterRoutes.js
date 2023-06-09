91217c0f71d17e5c857b112849f91183c28bf964
Angelique Uwimbabazi <uwangela12@gmail.com>
2023-06-09 12:57:28 +0200

    fixing deployment problems

--- Routes/userRegisterRoutes.js
+++ Routes/userRegisterRoutes.js
@@ -1,15 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
-import {SignUpController,Login, readUser} from "../Controllers/userRegisterController.js"
+// import {SignUpController,Login, readUser} from "../Controllers/userRegisterController.js"
 
 
 const router=express.Router();
 router.use(bodyParser.json())
 
 
-router.post("/signup",SignUpController)
-router.post("/Login",Login)
-router.get("/readUser", readUser)
+// router.post("/signup",SignUpController)
+// router.post("/Login",Login)
+// router.get("/readUser", readUser)
 
 
 export default router
