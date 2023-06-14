4f720ea551dbe5152f9171381cd6b6b261ef845f
Angelique Uwimbabazi <uwangela12@gmail.com>
2023-06-14 12:24:37 +0200

    home page

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import transferRoute from "./Routes/transferRoute.js"
 import addExpense from "./Routes/expenseRoute.js"
 import dotenv from "dotenv";
 
-import Home from "./Routes/userRegisterRoutes.js"
+import Home from "./Routes/homeRoute.js"
 import router from "./Routes/userRegisterRoutes.js";
 
 // import bankroute from "./routes/bankroute.js";
@@ -39,30 +39,8 @@ app.use(bodyParser.urlencoded({ extended: true })); // THis will help us to acce
 app.use('/api/v1', savingroute)
 app.use('/api/v1', bankroute)
 app.use('/api/v1', expenseRoute)
-
+app.use("/", Home)
 app.use("/api/v1/user", member)
-
-
-
-
-const connectToMongoDb = () => {
-    mongoose.connect(process.env.MONGOPASS)
-        .then(() => {
-            console.log("database connected succesfully");
-        }).catch((err) => {
-            console.log("failed to connect to the database", err)
-        })
-}
-const port = 5500;
-app.listen(port, () => {
-    console.log('server is running on port' + port)
-    connectToMongoDb();
-});
-
-
-
-
-
 app.get("/",(req, res) => {
     res.send("Welcome in our deployment page saving mentor! ")
 });
@@ -94,6 +72,29 @@ app.use(bodyParser.json())
 // app.use(bodyParser.urlencoded({ extended: true })); // THis will help us to access data from form 
 
 app.use("/api/v1/user", member)
-// app.use("/", Home)
+
+
+
+
+
+
+const connectToMongoDb = () => {
+    mongoose.connect(process.env.MONGOPASS)
+        .then(() => {
+            console.log("database connected succesfully");
+        }).catch((err) => {
+            console.log("failed to connect to the database", err)
+        })
+}
+const port = 5500;
+app.listen(port, () => {
+    console.log('server is running on port' + port)
+    connectToMongoDb();
+});
+
+
+
+
+
 
 
