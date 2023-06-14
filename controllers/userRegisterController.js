55f4d0e298f321100fd360185e88324b80401f70
Angelique Uwimbabazi <uwangela12@gmail.com>
2023-06-14 19:21:35 +0200

    fix node mail

--- controllers/userRegisterController.js
+++ controllers/userRegisterController.js
@@ -4,15 +4,14 @@ import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 import nodemailer from "nodemailer"
 
-
-
 import savingAccount from "../Models/savingModel.js"
-import bankAccouts  from "../Models/bankModel.js"
-import bcrypt  from "bcrypt"
+import bankAccouts from "../Models/bankModel.js"
 
 
-import jwt from "jsonwebtoken"
-const secretKey='@@key'//This is my secret key
+
+
+import { resourceLimits } from "worker_threads"
+const secretKey = '@@key'//This is my secret key
 
 
 const SignUpController = async (req, res) => {
@@ -39,8 +38,31 @@ const SignUpController = async (req, res) => {
                 Email: data.Email,
                 Username: data.Username,
                 Password: data.Password
+            });
+            const savedData = await userInfo.save();
+            if (savedData.length == 0) {
+                res.send("empty data")
+            };
+            /// THis code is about saving account
+            let saving = new savingAccount({
+                accountHolder: data.Email,
+                amount: 0
+            })
+            saving.save()
+            // THis is bank account
+            let bankAccount = new bankAccouts({
+                bankHolder: data.Email,
+                Amount: 800000
+            })
+            bankAccount.save()
+
+            const token = jwt.sign({ userId: savedData._id }, secretKey)
+            res.json({
+                message: "account successfully  created",
+                token: token,
 
             })
+
             const savingMantor = nodemailer.createTransport({
                 service: 'gmail',
                 host: 'smtp.gmail.com',
@@ -60,98 +82,23 @@ const SignUpController = async (req, res) => {
                 text: 'hey there',
                 html: 'thank you for joining the best communnity when it comes to saving, we love you ❤️',
             };
-           })
-          
-           const savedData = await userInfo.save();
-           if(savedData.length==0){
-            res.send("empty data")
-           }
-/// THis code is about saving account
-let saving=new savingAccount({
-    accountHolder:data.Email,
-    amount:0
-})
-saving.save()
-// THis is bank account
 
-import savingAccount from "../Models/savingModel.js"
-import bankAccouts from "../Models/bankModel.js"
-import bcrypt from "bcrypt"
-import jwt from "jsonwebtoken"
-const secretKey = '@@key'//This is my secret key
-
-const SignUpController = async (req, res) => {
-
-    const data = req.body
-    if (data.length === 0) {
-        return res.status(400).json({ message: "Empty data" });
-    }
-    const salt = await bcrypt.genSalt(8);
-    const hashedPassword = await bcrypt.hash(data.Password, salt);
-    data.Password = hashedPassword
-
-    const existinguser = await Users.findOne({ Email: data.Email })
-    if (existinguser) {
-        res.status(200).json({
-            message: "email already in use"
-        })
-
-    }
-    else {
-        let userInfo = new Users({
-            Firstname: data.Firstname,
-            Lastname: data.Lastname,
-            Email: data.Email,
-            Username: data.Username,
-            Password: data.Password
-        })
-
-        const savedData = await userInfo.save();
-        if (savedData.length == 0) {
-            res.send("empty data")
+            savingMantor.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('email sent', info.messageId);
+                }
+            })
         }
-        /// THis code is about saving account
-        let saving = new savingAccount({
-            accountHolder: data.Email,
-            amount: 0
-        })
-        saving.save()
-        // THis is bank account
-
-        let bankAccount = new bankAccouts({
-            bankHolder: data.Email,
-            Amount: 800000
-        })
-        bankAccount.save()
-
-        const token = jwt.sign({ userId: savedData._id }, secretKey)
-        res.json({
-            message: "account successfully  created",
-            token: token,
-
-        })
-    }
-
-
-
-
-
 
     }
+
     catch (err) {
         console.log("some error:", err)
         res.send("some error occured")
     }
 
-}
-const Login = async (req, res) => {
-    try {
-        const data = req.body
-        const existingUserEmail = await Users.findOne({ Email: data.Email })
-
-
-        if (existingUserEmail) {
-            const existingUserEmailPassword = await bcrypt.compare(data.Password, existingUserEmail.Password)
 
 }
 
@@ -162,25 +109,6 @@ const Login = async (req, res) => {
         const data = req.body
         const existingUserEmail = await Users.findOne({ Email: data.Email })
 
-            if (existingUserEmailPassword) {
-                const token = jwt.sign({ userId: existingUserEmail._id }, secretKey)
-
-                res.status(200).json({
-                    message: "Login succesful",
-                    token: token
-                })
-            } else {
-                res.status(200).json({
-                    message: "your password is incorrect"
-                })
-            }
-
-
-        }
-        else {
-
-            res.send("Create your account ,or check your credentials")
-        }
 
         if (existingUserEmail) {
             const existingUserEmailPassword = await bcrypt.compare(data.Password, existingUserEmail.Password)
@@ -203,64 +131,16 @@ const Login = async (req, res) => {
         else {
 
             res.send("Create your account ,or check your credentials")
-
-
-
-
         }
-
-
-        }
-        else {
-
-            res.send("Create your account ,or check your credentials")
-        }
-
     }
     catch (err) {
         console.log("some error:", err)
         res.send("some error occured")
     }
 
-
-    }
-    catch (err) {
-        console.log("some error:", err)
-        res.send("some error occured")
-    }
 
 
 }
-
-
-export const readUser = async (req, res) => {
-    try {
-        const response = await Users.find({})
-        if (response.length == 0) {
-            res.status(409).json({
-                message: "No data Found",
-                data: response,
-                error: "Data not found",
-            })
-        } else {
-            res.status(200).json({
-                message: "Users Found successfully",
-                error: null,
-                data: response
-            })
-        }
-    }
-    catch (err) {
-        console.log("error catched", err)
-        res.status(500).json({
-            message: "failed find data",
-            error: "failed"
-        
-
-
-
-
-
 
 
 
