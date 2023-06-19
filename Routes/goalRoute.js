6a4e3528a956cf9af63fdc8f7b45f78371bbabce
Angelique Uwimbabazi <uwangela12@gmail.com>
2023-06-19 18:32:12 +0200

    remove all controller importation error

--- Routes/goalRoute.js
+++ Routes/goalRoute.js
@@ -1,5 +1,5 @@
 import express from "express"
-import createGoal from "../Controllers/goalController.js"
+import createGoal from "../controllers/goalController.js"
 const router = express.Router();
 router.post("/setGoal" ,createGoal );
 export default router;
